09e0aead6ff4590e780ed405d103d1c79b9051aa
Vitor <vit.marinho2001@gmail.com>
2023-12-25 19:10:26 -0300

    StructureTheProject

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -8,6 +8,9 @@ dotenv.config();
 const port = process.env.PORT;
 
 app.use(express.json()); // Habilita o uso de JSON no corpo das requisições
+app.get('/', (req, res) => {
+  res.status(200).send('API funcionando');;
+});
 
 app.listen(port, cors(), () => {
   console.log(`Servidor rodando na porta ${port}`);
